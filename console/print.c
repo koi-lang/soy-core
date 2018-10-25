f715b03ff7f4accd2de492f00ec643705d820d46
DeflatedPickle <DeflatedPickle@gmail.com>
2018-10-25 08:07:51 +0100

    Added print procedures

---
+++ console/print.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void print(char* arg) {
+    printf("%s", arg);
+}
---
+++ console/println.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void println(char* arg) {
+    printf("%s", arg);
+    printf("\n");
+}
